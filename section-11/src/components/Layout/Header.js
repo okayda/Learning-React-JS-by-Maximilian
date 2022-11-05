54e3ecaf84d19e0d304c7edb7716a3050ba593a9
Jhon <okayda999@gmail.com>
2022-11-05 11:29:54 -0700

    Add files changes at section-10 & 11 and Add fixed problem at section-11

--- section-11/src/components/Layout/Header.js
+++ section-11/src/components/Layout/Header.js
@@ -1,18 +1,19 @@
-import { Frament } from "React";
+import { Fragment } from "react";
 import mealsImg from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
 const Header = function (props) {
   return (
-    <Frament className={classes.header}>
-      <header>
+    <Fragment>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button>Cart</button>
       </header>
+
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="A table full of delicious food!" />
       </div>
-    </Frament>
+    </Fragment>
   );
 };
 

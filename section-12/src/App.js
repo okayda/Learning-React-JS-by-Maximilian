a36192710382d9ea3e17fa0514137a5e2a90c164
Jhon <59811681+okayda@users.noreply.github.com>
2022-11-21 15:04:21 -0800

    Add Component updates in action at section-12

---
+++ section-12/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = function () {
+    setShowParagraph((prev) => !prev);
+  };
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;

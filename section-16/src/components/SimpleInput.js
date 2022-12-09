0b52508a02ceb2a7b647437d6e6090f760b4fbee
Jhon <59811681+okayda@users.noreply.github.com>
2022-12-08 17:48:08 -0800

    Add refactoring & deriving states at section-16

--- section-16/src/components/SimpleInput.js
+++ section-16/src/components/SimpleInput.js
@@ -1,16 +1,12 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      console.log("Name Input is valid!");
-    }
-  }, [enteredNameIsValid]);
+  const enteredNameIsValid = enteredName.trim("") !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = function (e) {
     setEnteredName(e.target.value);
@@ -18,11 +14,7 @@ const SimpleInput = (props) => {
 
   const nameInputBlurHandler = function (e) {
     setEnteredNameTouched(true);
-
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return;
-    }
+    console.log("Lol");
   };
 
   const formSubmissionHandler = function (e) {
@@ -30,23 +22,19 @@ const SimpleInput = (props) => {
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-
-    const enteredValue = nameInputRef.current.value;
+    // const enteredValue = nameInputRef.current.value;
     // nameInputRef.current.value = "";
     // Not ideal do not directly manipulate the dom
     // only react are the responsible for manipulation
-    console.log(enteredValue);
+    // console.log(enteredValue);
 
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"

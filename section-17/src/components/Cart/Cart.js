358a65ff1b7430f0266c313231a79b27db62ef66
Jhon <59811681+okayda@users.noreply.github.com>
2022-12-16 07:08:36 -0800

    Add adding a checkout form at section-17

--- section-17/src/components/Cart/Cart.js
+++ section-17/src/components/Cart/Cart.js
@@ -1,56 +1,67 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext);
+	const [isCheckout, setIsCheckout] = useState(false);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length > 0;
+	const cartCtx = useContext(CartContext);
 
-  const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id);
-  };
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+	const hasItems = cartCtx.items.length > 0;
 
-  const cartItemAddHandler = (item) => {
-    cartCtx.addItem(item);
-  };
+	const cartItemRemoveHandler = (id) => {
+		cartCtx.removeItem(id);
+	};
 
-  console.log(cartCtx.items);
+	const cartItemAddHandler = (item) => {
+		cartCtx.addItem(item);
+	};
 
-  const cartItems = (
-    <ul className={classes["cart-items"]}>
-      {cartCtx.items.map((item) => (
-        <CartItem
-          key={item.id}
-          name={item.name}
-          amount={item.amount}
-          price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item)}
-        />
-      ))}
-    </ul>
-  );
+	const orderHandler = function () {
+		setIsCheckout(true);
+	}
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
-    </Modal>
-  );
+
+	const cartItems = (
+		<ul className={classes["cart-items"]}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
+		</ul>
+	);
+
+	const modalActions = <div className={classes.actions}>
+		<button className={classes["button--alt"]} onClick={props.onClose}>
+			Close
+		</button>
+		{hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+	</div>
+
+	return (
+		<Modal onClose={props.onClose}>
+			{cartItems}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>{totalAmount}</span>
+			</div>
+
+			{isCheckout && <Checkout onCancel={props.onClose} />}
+
+			{!isCheckout && modalActions}
+		</Modal>
+	);
 };
 
 export default Cart;

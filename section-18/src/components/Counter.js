cdc93659db703bd57453981783aac1947951c0be
Jhon <59811681+okayda@users.noreply.github.com>
2022-12-19 10:00:51 -0800

    Add using redux data in react components at section-18

--- section-18/src/components/Counter.js
+++ section-18/src/components/Counter.js
@@ -1,12 +1,21 @@
+import { useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // Great thing about useSelector() React-redux will automatically
+  // set up a subscribtion to the redux store for this component
+
+  // basically whenever the counter data in the store (central data store)
+  // is change/updated this component will be automatically re-evaluated
+  // and update the U.I
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );

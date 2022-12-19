06d335796eaaf07404d09fae49d10cc5f486f88e
Jhon <59811681+okayda@users.noreply.github.com>
2022-12-19 07:53:00 -0800

    Add creating a redux store for react at section-18

---
+++ section-18/src/store/redux.js
@@ -0,0 +1,13 @@
+import { createStore } from "redux";
+
+const counterReducer = function (state = { counter: 0 }, action) {
+  if (action.type === "INCREMENT") return { counter: state.counter + 1 };
+
+  if (action.type === "DECREMENT") return { counter: state.counter - 1 };
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;

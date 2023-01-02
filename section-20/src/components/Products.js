27cfd3662aba5daebb2c8e5325e48826b315ae99
Jhon <59811681+okayda@users.noreply.github.com>
2023-01-02 14:20:48 -0800

    Add defining & using routes at section-20

---
+++ section-20/src/components/Products.js
@@ -0,0 +1,5 @@
+const Products = function () {
+  return <h1>The Products Page</h1>;
+};
+
+export default Products;
---
+++ section-20/src/components/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = function () {
+  return <h1>The Welcome Page</h1>;
+};
+
+export default Welcome;
